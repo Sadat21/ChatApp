5d71ddbc4fb66747ba4612626f13dd8209af0b2a
Sadat Islam <sadat21@hotmail.ca>
2020-03-07 11:40:36 -0700

    Create basic UI

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import * as S from "./App.styles";
+import { Grid, TextareaAutosize, TextField } from "@material-ui/core";
 
 function App() {
   return (
     <S.Content>
-      <h1>Hello Word</h1>
+      <Grid container>
+        <Grid item xs={8}>
+          <h1>You are User1</h1>
+          <TextField
+            style={{ width: "100%" }}
+            multiline
+            variant={"outlined"}
+            rows="20"
+            disabled
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <h1>Online Users</h1>
+          <TextField
+            style={{ width: "100%" }}
+            multiline
+            variant={"outlined"}
+            rows="20"
+            disabled
+          />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12}>
+          <h1>Chat Box</h1>
+          <TextField style={{ width: "100%" }} variant={"outlined"} />
+        </Grid>
+      </Grid>
     </S.Content>
   );
 }
---
+++ src/redux/models/User.tsx
@@ -0,0 +1,4 @@
+export class User {
+  nickname: string = "";
+  colour: string = "";
+}
