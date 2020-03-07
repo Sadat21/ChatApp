84366b9a97e34f8ae953fa2ea93fda8fe3b2c5ff
Sadat Islam <sadat21@hotmail.ca>
2020-03-07 11:11:18 -0700

    Install Material Ui

---
+++ src/components/App/App.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  overflow-x: hidden;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+`;
