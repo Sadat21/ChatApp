75921cdaef70f0eec1aeec9fc4798a66fb2e2f01
Sadat Islam <sadat21@hotmail.ca>
2020-03-07 11:06:45 -0700

    Setup Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Reducer } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from './components/App/App'
+import thunkMiddleware from "redux-thunk";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, Store } from "redux";
+import combinedReducer, { ReduxState } from "./redux/combindedReducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const reducer: Reducer<ReduxState, any> = combinedReducer;
+
+// @ts-ignore
+const store: Store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+const render = (Component: any): void => {
+  return ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+
+render(App);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/redux/combindedReducer.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+export interface ReduxState {
+
+}
+
+const combinedReducer = combineReducers({
+
+});
+
+export default combinedReducer;
