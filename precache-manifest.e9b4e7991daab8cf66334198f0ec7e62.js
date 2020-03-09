61d8bf4d178045c1d3fbd15fd8a365945ba50f43
Sadat Islam <sadat21@hotmail.ca>
2020-03-09 08:29:17 -0600

    Updates

---
+++ precache-manifest.e9b4e7991daab8cf66334198f0ec7e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daba5e618d980b1db4e9a986901edb80",
+    "url": "/ChatAppFrontend/index.html"
+  },
+  {
+    "revision": "ec2a8c4e16f6532e78e4",
+    "url": "/ChatAppFrontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5d532b3ebecee32c3361",
+    "url": "/ChatAppFrontend/static/js/2.0e7ef00d.chunk.js"
+  },
+  {
+    "revision": "924033e0f4b246eac2a0f753ebba571a",
+    "url": "/ChatAppFrontend/static/js/2.0e7ef00d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2a8c4e16f6532e78e4",
+    "url": "/ChatAppFrontend/static/js/main.4e856270.chunk.js"
+  },
+  {
+    "revision": "916cc9eba92ea2c8e91d",
+    "url": "/ChatAppFrontend/static/js/runtime-main.b7449c2c.js"
+  }
+]);
